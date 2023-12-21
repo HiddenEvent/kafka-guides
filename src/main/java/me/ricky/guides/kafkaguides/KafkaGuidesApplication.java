4231e65b210f412b4ce278d35eda3617008ed8d4
RichardKim <hiddeneventor@gmail.com>
2023-12-21 23:48:48 +0900

    Initial commit

---
+++ src/main/java/me/ricky/guides/kafkaguides/KafkaGuidesApplication.java
@@ -0,0 +1,13 @@
+package me.ricky.guides.kafkaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGuidesApplication.class, args);
+    }
+
+}
