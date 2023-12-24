8bf802cf1fbe4434f5d8737b0b59cf22490636e1
RichardKim <hiddeneventor@gmail.com>
2023-12-24 17:11:51 +0900

    [Producer 사용법] Controller => 이벤트 발행

--- src/main/java/me/ricky/guides/kafkaguides/controller/LibraryEventController.java
+++ src/main/java/me/ricky/guides/kafkaguides/controller/LibraryEventController.java
@@ -1,7 +1,9 @@
 package me.ricky.guides.kafkaguides.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import me.ricky.guides.kafkaguides.domain.LibraryEvent;
+import me.ricky.guides.kafkaguides.producer.LibraryEventsProducer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class LibraryEventController {
+
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
+
     @PostMapping("/v1/libraryevent")
-    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public LibraryEvent postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("libraryEvent: {}", libraryEvent);
+        libraryEventsProducer.sendLibraryEvent(libraryEvent);
         return libraryEvent;
     }
 }
