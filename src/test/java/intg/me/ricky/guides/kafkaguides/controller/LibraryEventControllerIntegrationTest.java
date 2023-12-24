08f8e8179789029e06c2494f272d95cf4839028c
RichardKim <hiddeneventor@gmail.com>
2023-12-24 23:38:53 +0900

    [Kafka 테스트] 비동기 방식 테스트 1. consumer 생성 2. EmbeddedKafkaBroker에 consumer 등록 3. KafkaTestUtils.getRecords(consumer) 를 통해 topic에 해당하는 모든 레코드 검색 4. 레코드 검증

--- src/test/java/intg/me/ricky/guides/kafkaguides/controller/LibraryEventControllerIntegrationTest.java
+++ src/test/java/intg/me/ricky/guides/kafkaguides/controller/LibraryEventControllerIntegrationTest.java
@@ -1,16 +1,30 @@
 package me.ricky.guides.kafkaguides.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import me.ricky.guides.kafkaguides.domain.LibraryEvent;
 import me.ricky.guides.kafkaguides.util.TestUtil;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,6 +36,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LibraryEventControllerIntegrationTest {
     @Autowired
     TestRestTemplate restTemplate;
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        // consumer 생성 (테스트)
+        consumer = new DefaultKafkaConsumerFactory<>(consumerProps, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        // EmbeddedKafkaBroker 에 테스트 consumer 등록
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        // consumer 종료
+        consumer.close();
+    }
 
     @Test
     void postLibraryEvent() {
@@ -37,5 +75,16 @@ class LibraryEventControllerIntegrationTest {
         //then
         log.info("response 데이터 확인: {}", response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        // 비동기 방식 검증 처리 => blocking 하여 Kafka message consumer Records를 가져온다.
+        ConsumerRecords<Integer, String> consumerRecords = KafkaTestUtils.getRecords(consumer);
+        assertThat(consumerRecords.count()).isEqualTo(1);
+
+        consumerRecords.forEach(record -> {
+            LibraryEvent libraryEventActual = TestUtil.parseLibraryEventRecord(objectMapper, record.value());
+            System.out.println("libraryEventActual: " + libraryEventActual);
+            assertThat(libraryEventActual).isEqualTo(libraryEvent);
+        });
+
     }
 }
